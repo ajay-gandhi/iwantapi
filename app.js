be24003d64485452baa46725f721c91cfc410aba
Ajay Gandhi <aag255@cornell.edu>
2016-02-08 14:17:29 -0500

    Done except data

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 
-var express = require('express');
+var express = require('express'),
+    rp      = require('request-promise');
 
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
-  res.send('This works!');
+  if (!req.query.callback) return res.send('Missing `callback` parameter');
+  if (!req.query.url)      return res.send('Missing `url` parameter');
+
+  var options = {
+    url: req.query.url
+  }
+  if (req.query.data) options.qs = JSON.parse(req.query.data);
+ 
+  rp(options)
+    .then(function (results) {
+      res.send(req.query.callback + '(' + JSON.stringify(results) + ')');
+    })
+    .catch(function () {
+      // Just drop errors
+    });
 });
 
-app.listen(process.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Listening on port', process.env.PORT || 5000);
 });
